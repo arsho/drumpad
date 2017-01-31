a2604fd04dbf79357fa4e8b857bd5645597a4a1b
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2017-01-31 18:39:25 +0600

    Updated drumpad application
    add keyboard button press event

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,48 +1,40 @@
 $(document).ready(function () {
     function play_sound($sound_id) {
+		$("td").removeClass('drum_pad_hover');
         $($sound_id).trigger("play");
         $($sound_id)[0].currentTime = 0;
+		$element_name = $sound_id+"_pad";
+		$($element_name).addClass("drum_pad_hover");
+		window.setTimeout(function() {
+			$($element_name).removeClass('drum_pad_hover');			
+		}, 300);		
     }
+	var sound_ar = [];
+	sound_ar["a"] = "cymbal1";
+	sound_ar["s"] = "tom1";
+	sound_ar["d"] = "tom2";
+	sound_ar["f"] = "cymbal2";
+	
+	sound_ar["j"] = "bass";
+	sound_ar["k"] = "snare";
+	sound_ar["l"] = "hihat1";
+	sound_ar[";"] = "hihat2";
+		
+	$(window).keypress(function(e){
+		e = e || window.event;
+		var key = e.keyCode || e.which;
+		key = String.fromCharCode(key);
+		key_lower = key.toLowerCase();
+		
+		if (key_lower in sound_ar){
+			$pass_id = "#" + sound_ar[key_lower];
+			play_sound($pass_id);			
+		}
+	});
 
     $("td").on("click", function () {
         $this_id = $(this).attr('id');
         $pass_id = "#" + $this_id.split("_")[0];
         play_sound($pass_id);
-    });
-
-	/*Click function for my facebook link*/
-	$('#rib').click(function(){
-		window.location.assign("https://www.facebook.com/ars.shovon")
-	});
-  // Tooltip only Text
-    $('#rib , #showplaygame, #player1name').hover(function(){
-        // Hover over code
-        var title = $(this).attr('title');
-		if(title!=""){
-        $(this).data('tipText', title).removeAttr('title');
-        $('<p class="tooltip"></p>')
-        .text(title)
-        .appendTo('body')
-        .fadeIn('slow');
-    }}, function() {
-        // Hover out code
-        $(this).attr('title', $(this).data('tipText'));
-        $('.tooltip').remove();
-    }).mousemove(function(e) {
-    	var s=$(this).attr('id');
-    	if(s=="rib"){
-            var mousex = e.pageX -140; //Get X coordinates
-            var mousey = e.pageY+35; //Get Y coordinates
-        }
-        else
-        {
-            var mousex = e.pageX -65; //Get X coordinates
-            var mousey = e.pageY+22; //Get Y coordinates            	
-        }
-            $('.tooltip')
-            .css({ top: mousey, left: mousex })
-			
-    });
-
-    
+    });    
 });
